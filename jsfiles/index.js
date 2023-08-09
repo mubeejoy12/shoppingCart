2c9b3d9e288b241467bf96d0cd1492ed7d9f094c
samee185 <samueliy185@gmail.com>
2023-08-09 18:26:31 +0800

    shopping cart navbar

---
+++ jsfiles/index.js
@@ -0,0 +1,8 @@
+const menuBar = document.querySelector('#menuIcon')
+const dropDown = document.querySelector('#drop')
+function shoppingMenu(){
+dropDown.style.display = 'none'
+menuBar.classList.toggle('navDrop')
+}
+
+ 
